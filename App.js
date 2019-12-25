2b59b668c94f2345dccb325172141d24ff3b710d
lbojanic <lazar.bojanic@hotmail.rs>
2019-12-25 20:07:05 +0100

    Added home screen layout

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoggedOut from './src/screens/LoggedOut'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Proba!</Text>
-    </View>
-  );
+export default class App extends Component<Props> {
+  render() {
+    return <LoggedOut />
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/LoggedOut.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import { render } from 'react-dom';
+import colors from '../styles/colors'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import RoundedButton from '../components/buttons/RoundedButton'
+import { TouchableHighlight } from 'react-native-gesture-handler';
+
+export default class LoggedOut extends Component {
+    onFacebookPress() {
+        alert('Facebook button pressed')
+    }
+    onCreateAccountPress() {
+        alert('Create Account button pressed')
+    }
+    onMoreOptionsPressed() {
+        alert('More options button pressed')
+    }
+    render() {
+        return (
+        <View style={styles.wrapper}>
+            <View style={styles.welcomeWrapper}>
+                <Image 
+                    source={require('../img/logo.png')}
+                    style={styles.logo}
+                />
+                <Text style={styles.welcomeText}>Welcome to Vozi.</Text>
+                <RoundedButton 
+                    text="Continue with Facebook"
+                    textColor={colors.green01}
+                    background={colors.white}
+                    icon={<Icon name="facebook" size={20} style={styles.facebookButtonIcon}/>}
+                    handleOnPress={this.onFacebookPress}
+                />
+                <RoundedButton 
+                    text="Create Account"
+                    textColor={colors.white}
+                    handleOnPress={this.onCreateAccountPress}
+                />
+
+                <TouchableHighlight 
+                    style={styles.moreOptionsButton}
+                    onPress={this.onMoreOptionsPressed}>
+                    <Text style={styles.moreOptionsButtonText}>More options</Text>
+                </TouchableHighlight>
+
+                <View style={styles.termsAndConditions}>
+                    <Text style={styles.termsText}> By tapping Continue, Create Account or More options,</Text>
+                    <Text style={styles.termsText}> I agree to LB's </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Terms of Service</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>, </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Payments Terms of Service</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>, </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Privacy Policy</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>, and </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Nondiscrimination policy</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>.</Text>
+                </View>
+            </View>
+        </View>
+            );
+    }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        display: 'flex',
+        backgroundColor: colors.green01,
+    },
+    welcomeWrapper: {
+        flex: 1,
+        display: 'flex',
+        marginTop: 30, 
+        padding: 20,
+    },
+    logo: {
+        width: 200,
+        height: 50,
+        marginTop: 50,
+        marginBottom: 40,
+    },
+    welcomeText: {
+        fontSize: 30,
+        color: colors.white,
+        fontWeight: '400', 
+        marginBottom: 40,
+    },
+    facebookButtonIcon: {
+        color: colors.green01,
+        position: 'relative',
+        left: 20,
+        zIndex: 8,
+    },
+    moreOptionsButton: {
+        marginTop: 10,
+    },
+    moreOptionsButtonText: {
+        color: colors.white,
+        fontSize: 16,
+    },
+    termsAndConditions: {
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        marginTop: 30,
+    },
+    termsText: {
+        color: colors.white,
+        fontSize: 13,
+        fontWeight: '600',
+    },
+    linkButton: {
+        borderBottomWidth: 1,
+        borderBottomColor: colors.white,
+    }
+});
